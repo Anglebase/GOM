6bda7d582d394e9408439f78aa1ba8347ee46ca6
Anglebase <1628818324@qq.com>
2025-02-06 14:11:35 +0800

    feat: 添加constcat宏的导入

--- src/lib.rs
+++ src/lib.rs
@@ -136,6 +136,10 @@ impl<T: Send + 'static> Registry<T> {
     }
 }
 
+#[allow(unused_imports)]
+#[macro_use]
+extern crate constcat;
+
 /// Make a identifier string with the given path
 ///
 /// ```rust
